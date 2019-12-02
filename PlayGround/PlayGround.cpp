c20aa76f59bbe2464cffab0db80ecc17e1e7222d
amn <jxw9391@qq.com>
2019-12-03 00:05:57 +0800

    load and display default model for the demo

--- PlayGround/PlayGround.cpp
+++ PlayGround/PlayGround.cpp
@@ -1,15 +1,85 @@
 #include "PlayGround.h"
 
+const std::string PlayGround::skDefaultModel = "Arcade/Arcade.fbx";
+
+CameraController& PlayGround::getActiveCameraController()
+{
+    return mFirstPersonCameraController;
+}
+
 void PlayGround::onLoad(SampleCallbacks* pSample, RenderContext* pRenderContext)
 {
+    mpCamera = Camera::create();
+    mpProgram = GraphicsProgram::createFromFile("ModelViewer.ps.hlsl", "", "main");
 
+    mModelViewCameraController.attachCamera(mpCamera);
+    mFirstPersonCameraController.attachCamera(mpCamera);
+    m6DoFCameraController.attachCamera(mpCamera);
+
+    //create sampler
+    Sampler::Desc samplerDesc;
+    samplerDesc.setFilterMode(Sampler::Filter::Point, Sampler::Filter::Point, Sampler::Filter::Point);
+    mpPointSampler = Sampler::create(samplerDesc);
+    samplerDesc.setFilterMode(Sampler::Filter::Linear, Sampler::Filter::Linear, Sampler::Filter::Linear);
+    mpLinearSampler = Sampler::create(samplerDesc);
+
+    mpDirLight = DirectionalLight::create();
+    mpPointLight = PointLight::create();
+    mpDirLight->setWorldDirection(glm::vec3(0.13f, 0.27f, -0.9f));
+
+    mpProgramVars = GraphicsVars::create(mpProgram->getReflector());
+    mpGraphicsState = GraphicsState::create();
+    mpGraphicsState->setProgram(mpProgram);
+
+    //load model
+    mpModel = Model::createFromFile(skDefaultModel.c_str(), Model::LoadFlags::None);
+
+
+    mpModel->bindSamplerToMaterials(mUseTriLinearFiltering ? mpLinearSampler : mpPointSampler);
 }
 
 void PlayGround::onFrameRender(SampleCallbacks* pSample, RenderContext* pRenderContext, const Fbo::SharedPtr& pTargetFbo)
 {
+    const glm::vec4 clearColor(0.38f, 0.52f, 0.10f, 1);
+    pRenderContext->clearFbo(pTargetFbo.get(), clearColor, 1.0f, 0, FboAttachmentType::All);
+    mpGraphicsState->setFbo(pTargetFbo);
 
+    if (mpModel)
+    {
+        mpCamera->setDepthRange(0.1f, mpModel->getRadius()*10.0f);
+        mFirstPersonCameraController.update();
+
+        mpProgramVars["PerFrameCB"]["gConstColor"] = false;
+
+        ConstantBuffer* pCB = mpProgramVars["PerFrameCB"].get();
+        mpDirLight->setIntoProgramVars(mpProgramVars.get(), pCB, "gDirLight");
+        mpPointLight->setIntoProgramVars(mpProgramVars.get(), pCB, "gPointLight");
+
+        mpGraphicsState->setProgram(mpProgram);
+        pRenderContext->setGraphicsState(mpGraphicsState);
+        pRenderContext->setGraphicsVars(mpProgramVars);
+        ModelRenderer::render(pRenderContext, mpModel, mpCamera.get());
+    }
 }
 
+
+bool PlayGround::onKeyEvent(SampleCallbacks* pSample, const KeyboardEvent& keyEvent)
+{
+    bool bHandled = getActiveCameraController().onKeyEvent(keyEvent);
+    if (bHandled == false)
+    {
+        if (keyEvent.type == KeyboardEvent::Type::KeyPressed)
+        {
+
+        }
+    }
+    return bHandled;
+}
+
+bool PlayGround::onMouseEvent(SampleCallbacks* pSample, const MouseEvent& mouseEvent)
+{
+    return getActiveCameraController().onMouseEvent(mouseEvent);
+}
 
 #ifdef _WIN32
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
--- PlayGround/PlayGround.h
+++ PlayGround/PlayGround.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Falcor.h"
 #include "FalcorExperimental.h"
+#include "Utils/Picking/Picking.h"
 
 using namespace Falcor;
 
@@ -9,4 +10,28 @@ class PlayGround : public Renderer
 public:
     void onLoad(SampleCallbacks* pSample, RenderContext* pRenderContext) override;
     void onFrameRender(SampleCallbacks* pSample, RenderContext* pRenderContext, const Fbo::SharedPtr& pTargetFbo) override;
+    bool onKeyEvent(SampleCallbacks* pSample, const KeyboardEvent& keyEvent) override;
+    bool onMouseEvent(SampleCallbacks* pSample, const MouseEvent& mouseEvent) override;
+
+private:
+    Camera::SharedPtr mpCamera;
+    RasterizerState::SharedPtr mpWireframeRS = nullptr;
+
+    bool mUseTriLinearFiltering = true;
+    Sampler::SharedPtr mpPointSampler = nullptr;
+    Sampler::SharedPtr mpLinearSampler = nullptr;
+
+    static const std::string skDefaultModel;
+    Model::SharedPtr mpModel = nullptr;
+    ModelViewCameraController mModelViewCameraController;
+    FirstPersonCameraController mFirstPersonCameraController;
+    SixDoFCameraController m6DoFCameraController;
+
+    DirectionalLight::SharedPtr mpDirLight;
+    PointLight::SharedPtr mpPointLight;
+
+    GraphicsProgram::SharedPtr mpProgram = nullptr;
+    GraphicsVars::SharedPtr mpProgramVars = nullptr;
+    GraphicsState::SharedPtr mpGraphicsState = nullptr;
+    CameraController& getActiveCameraController();
 };
