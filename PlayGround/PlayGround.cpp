1f996bb88521bdae74c72fdc437bc1f14b874a46
amn <jxw9391@qq.com>
2020-01-09 00:29:49 +0800

    add ray-trace feature in demo, add a switch to choose ray-trace.

--- PlayGround/PlayGround.cpp
+++ PlayGround/PlayGround.cpp
@@ -1,14 +1,43 @@
 #include "PlayGround.h"
 
-const std::string PlayGround::skDefaultModel = "Arcade/Arcade.fbx";
+static const glm::vec4 kClearColor(0.38f, 0.52f, 0.10f, 1);
+static const std::string skDefaultModel = "Arcade/Arcade.fscene";
 
 CameraController& PlayGround::getActiveCameraController()
 {
     return mModelViewCameraController;
 }
 
+void PlayGround::setPerFrameVars(const Fbo* pTargetFbo)
+{
+    PROFILE("setPerFrameVars");
+    GraphicsVars* pVars = mpRtVars->getGlobalVars().get();
+    ConstantBuffer::SharedPtr pCB = pVars->getConstantBuffer("PerFrameCB");
+    pCB["invView"] = glm::inverse(mpCamera->getViewMatrix());
+    pCB["viewportDims"] = vec2(pTargetFbo->getWidth(), pTargetFbo->getHeight());
+    float fovY = focalLengthToFovY(mpCamera->getFocalLength(), Camera::kDefaultFrameHeight);
+    pCB["tanHalfFovY"] = tanf(fovY * 0.5f);
+}
+
 void PlayGround::onLoad(SampleCallbacks* pSample, RenderContext* pRenderContext)
 {
+    if (gpDevice->isFeatureSupported(Device::SupportedFeatures::Raytracing) == false)
+    {
+        logErrorAndExit("Device does not support raytracing!", true);
+    }
+
+    RtProgram::Desc rtProgDesc;
+    rtProgDesc.addShaderLibrary("PlayGround.rt.hlsl").setRayGen("rayGen");
+    rtProgDesc.addHitGroup(0, "primaryClosestHit", "").addMiss(0, "primaryMiss");
+    rtProgDesc.addHitGroup(1, "", "shadowAnyHit").addMiss(1, "shadowMiss");
+
+    mpRaytraceProgram = RtProgram::create(rtProgDesc);
+
+    mpRtState = RtState::create();
+    mpRtState->setProgram(mpRaytraceProgram);
+    mpRtState->setMaxTraceRecursionDepth(3); // 1 for calling TraceRay from RayGen, 1 for calling it from the primary-ray ClosestHitShader for reflections, 1 for reflection ray tracing a shadow ray
+
+
     mpCamera = Camera::create();
     mpProgram = GraphicsProgram::createFromFile("PlayGround.ps.hlsl", "", "main");
 
@@ -27,12 +56,18 @@ void PlayGround::onLoad(SampleCallbacks* pSample, RenderContext* pRenderContext)
     mpPointLight = PointLight::create();
     mpDirLight->setWorldDirection(glm::vec3(0.13f, 0.27f, -0.9f));
 
+    //load model
+    mpScene = RtScene::loadFromFile(skDefaultModel.c_str(), RtBuildFlags::None, Model::LoadFlags::RemoveInstancing);
+    mpModel = mpScene->getModel(0);
+
+
+    mpRtVars = RtProgramVars::create(mpRaytraceProgram, mpScene);
+    mpRtRenderer = RtSceneRenderer::create(mpScene);
+
     mpProgramVars = GraphicsVars::create(mpProgram->getReflector());
     mpGraphicsState = GraphicsState::create();
     mpGraphicsState->setProgram(mpProgram);
 
-    //load model
-    mpModel = Model::createFromFile(skDefaultModel.c_str(), Model::LoadFlags::None);
     if (mpModel == nullptr)
     {
         msgBox("Could not load model");
@@ -53,26 +88,34 @@ void PlayGround::onFrameRender(SampleCallbacks* pSample, RenderContext* pRenderC
 {
     const glm::vec4 clearColor(0.38f, 0.52f, 0.10f, 1);
     pRenderContext->clearFbo(pTargetFbo.get(), clearColor, 1.0f, 0, FboAttachmentType::All);
-    mpGraphicsState->setFbo(pTargetFbo);
+   
 
     if (mpModel)
     {
+        mpGraphicsState->setFbo(pTargetFbo);
+
         mpCamera->setDepthRange(mNearZ, mFarZ);
         getActiveCameraController().update();
 
-        mpProgramVars["PerFrameCB"]["gConstColor"] = false;
-
-        ConstantBuffer* pCB = mpProgramVars["PerFrameCB"].get();
-        mpDirLight->setIntoProgramVars(mpProgramVars.get(), pCB, "gDirLight");
-        mpPointLight->setIntoProgramVars(mpProgramVars.get(), pCB, "gPointLight");
-
-        mpGraphicsState->setProgram(mpProgram);
-        pRenderContext->setGraphicsState(mpGraphicsState);
-        pRenderContext->setGraphicsVars(mpProgramVars);
-        ModelRenderer::render(pRenderContext, mpModel, mpCamera.get());
+        if (mRayTrace)
+            renderRayTrace(pRenderContext, pTargetFbo.get());
+        else
+            renderRater(pRenderContext);
     }
 }
 
+
+void PlayGround::onResizeSwapChain(SampleCallbacks* pSample, uint32_t width, uint32_t height)
+{
+    float h = (float)height;
+    float w = (float)width;
+
+    mpCamera->setFocalLength(18);
+    float aspectRatio = (w / h);
+    mpCamera->setAspectRatio(aspectRatio);
+
+    mpRtOut = Texture::create2D(width, height, ResourceFormat::RGBA16Float, 1, 1, nullptr, Resource::BindFlags::UnorderedAccess | Resource::BindFlags::ShaderResource);
+}
 
 bool PlayGround::onKeyEvent(SampleCallbacks* pSample, const KeyboardEvent& keyEvent)
 {
@@ -96,6 +139,41 @@ bool PlayGround::onKeyEvent(SampleCallbacks* pSample, const KeyboardEvent& keyEv
 bool PlayGround::onMouseEvent(SampleCallbacks* pSample, const MouseEvent& mouseEvent)
 {
     return getActiveCameraController().onMouseEvent(mouseEvent);
+}
+
+void PlayGround::onGuiRender(SampleCallbacks* pSample, Gui* pGui)
+{
+    pGui->addCheckBox("Ray Trace", mRayTrace);
+    if (pGui->addButton("Reset Camera"))
+    {
+        resetCamera();
+    }
+}
+
+void PlayGround::renderRater(RenderContext* pRenderContext)
+{
+    mpProgramVars["PerFrameCB"]["gConstColor"] = false;
+
+    ConstantBuffer* pCB = mpProgramVars["PerFrameCB"].get();
+    mpDirLight->setIntoProgramVars(mpProgramVars.get(), pCB, "gDirLight");
+    mpPointLight->setIntoProgramVars(mpProgramVars.get(), pCB, "gPointLight");
+
+    mpGraphicsState->setProgram(mpProgram);
+    pRenderContext->setGraphicsState(mpGraphicsState);
+    pRenderContext->setGraphicsVars(mpProgramVars);
+    ModelRenderer::render(pRenderContext, mpModel, mpCamera.get());
+}
+
+void PlayGround::renderRayTrace(RenderContext* pRenderContext, Fbo* pTargetFbo)
+{
+    PROFILE("renderRT");
+    setPerFrameVars(pTargetFbo);
+
+    pRenderContext->clearUAV(mpRtOut->getUAV().get(), kClearColor);
+    mpRtVars->getRayGenVars()->setTexture("gOutput", mpRtOut);
+
+    mpRtRenderer->renderScene(pRenderContext, mpRtVars, mpRtState, uvec3(pTargetFbo->getWidth(), pTargetFbo->getHeight(), 1), mpCamera.get());
+    pRenderContext->blit(mpRtOut->getSRV(), pTargetFbo->getRenderTargetView(0));
 }
 
 void PlayGround::resetCamera()
--- PlayGround/PlayGround.h
+++ PlayGround/PlayGround.h
@@ -10,10 +10,11 @@ class PlayGround : public Renderer
 public:
     void onLoad(SampleCallbacks* pSample, RenderContext* pRenderContext) override;
     void onFrameRender(SampleCallbacks* pSample, RenderContext* pRenderContext, const Fbo::SharedPtr& pTargetFbo) override;
+    void onResizeSwapChain(SampleCallbacks* pSample, uint32_t width, uint32_t height) override;
     bool onKeyEvent(SampleCallbacks* pSample, const KeyboardEvent& keyEvent) override;
     bool onMouseEvent(SampleCallbacks* pSample, const MouseEvent& mouseEvent) override;
+    void onGuiRender(SampleCallbacks* pSample, Gui* pGui) override;
 
-    void resetCamera();
 private:
     Camera::SharedPtr mpCamera;
     RasterizerState::SharedPtr mpWireframeRS = nullptr;
@@ -22,19 +23,33 @@ private:
     Sampler::SharedPtr mpPointSampler = nullptr;
     Sampler::SharedPtr mpLinearSampler = nullptr;
 
-    static const std::string skDefaultModel;
+    RtScene::SharedPtr mpScene;
     Model::SharedPtr mpModel = nullptr;
-    ModelViewCameraController mModelViewCameraController;
+    ModelViewCameraController mModelViewCameraController;  
     FirstPersonCameraController mFirstPersonCameraController;
     SixDoFCameraController m6DoFCameraController;
-
+     
     DirectionalLight::SharedPtr mpDirLight;
     PointLight::SharedPtr mpPointLight;
 
     GraphicsProgram::SharedPtr mpProgram = nullptr;
     GraphicsVars::SharedPtr mpProgramVars = nullptr;
     GraphicsState::SharedPtr mpGraphicsState = nullptr;
+
+    RtProgram::SharedPtr mpRaytraceProgram = nullptr;
+    RtProgramVars::SharedPtr mpRtVars;
+    RtState::SharedPtr mpRtState;
+    RtSceneRenderer::SharedPtr mpRtRenderer;
+    Texture::SharedPtr mpRtOut;
+
     CameraController& getActiveCameraController();
+    void setPerFrameVars(const Fbo* pTargetFbo);
+    void renderRater(RenderContext* pRenderContext);
+    void renderRayTrace(RenderContext* pRenderContext, Fbo* pTargetFbo);
+    void resetCamera();
+
     float mNearZ;
     float mFarZ;
+
+    bool mRayTrace=true;
 };
